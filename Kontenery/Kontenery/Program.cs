0889327474889750b140eeec2507addc5f08c15b
s27523 <s27523@pjwstk.edu.pl>
2024-03-15 16:48:28 +0100

    Dodanie klasy bazowej Kontener oraz wyjatku OverfillException

--- Kontenery/Kontenery/Program.cs
+++ Kontenery/Kontenery/Program.cs
@@ -1,1 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Kontenery;
+
+public abstract class Kontener
+{
+    protected double masaLadunku;
+    protected double wysokosc;
+    protected double wagaWlasna;
+    protected double glebokosc;
+    protected string serialNumber;
+    protected double maxLadownosc;
+
+    protected Kontener(double masaLadunku, double wysokosc, double wagaWlasna, double glebokosc, string serialNumber, double maxLadownosc)
+    {
+        this.masaLadunku = masaLadunku;
+        this.wysokosc = wysokosc;
+        this.wagaWlasna = wagaWlasna;
+        this.glebokosc = glebokosc;
+        this.serialNumber = serialNumber;
+        this.maxLadownosc = maxLadownosc;
+    }
+    
+    protected void OproznijLadunek()        //moze bd musiec zwracac      
+    {
+        masaLadunku = 0;
+    }
+
+    protected void ZaladujKontener(double masaLadunku)
+    {
+        //to ma dodac wartosc do aktualnej czy zaladowac od konca?
+        if (masaLadunku > maxLadownosc)
+        {
+            throw new OverfillException("Zbyt duzy ladunek! Kontener ma mniejsza ladownosc!");
+        }
+        this.masaLadunku = masaLadunku;
+    }
+}
